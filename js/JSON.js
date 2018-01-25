6fb010948d5004e4bd221c00c70ed29367749fb4
Sharon <smohammed@marketingassociates.com>
2018-01-25 15:35:44 -0500

    Added JSON file

---
+++ js/JSON.js
@@ -0,0 +1,12 @@
+var thePets = [
+    {
+        "name": "Meowsalot",
+        "species": "cat",
+        "favFood": "tuna"
+    },
+    {
+        "name": "Barky",
+        "species": "dog",
+        "favFood": "carrots"
+    }
+]
